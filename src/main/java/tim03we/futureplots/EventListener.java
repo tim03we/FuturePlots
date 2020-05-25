7bfe18ad6697be2d5e2e3e6876f6e1f2e71e29bd
Tim W <tim.wenderholm@yahoo.com>
2020-05-25 17:56:41 +0200

    fix tnt explosion

--- src/main/java/tim03we/futureplots/EventListener.java
+++ src/main/java/tim03we/futureplots/EventListener.java
@@ -17,20 +17,30 @@ package tim03we.futureplots;
  */
 
 import cn.nukkit.Player;
+import cn.nukkit.block.Block;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockBreakEvent;
 import cn.nukkit.event.block.BlockPlaceEvent;
+import cn.nukkit.event.block.BlockSpreadEvent;
+import cn.nukkit.event.entity.EntityExplodeEvent;
+import cn.nukkit.event.entity.EntityExplosionPrimeEvent;
 import cn.nukkit.event.entity.EntityShootBowEvent;
 import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.event.player.PlayerMoveEvent;
 import cn.nukkit.item.ItemEdible;
+import cn.nukkit.level.Location;
+import cn.nukkit.level.Position;
 import tim03we.futureplots.events.PlotBlockEvent;
 import tim03we.futureplots.events.PlotEnterEvent;
 import tim03we.futureplots.events.PlotEvent;
 import tim03we.futureplots.events.PlotLeaveEvent;
 import tim03we.futureplots.utils.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventListener extends Language implements Listener {
 
@@ -134,6 +144,22 @@ public class EventListener extends Language implements Listener {
                     }
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void onExplosion(EntityExplodeEvent event) {
+        Entity entity = event.getEntity();
+        if(event.isCancelled()) return;
+        if(Settings.levels.contains(entity.getLevel().getName())) {
+            ArrayList<Block> allowedBlocks = new ArrayList<>();
+            for (Block block : event.getBlockList()) {
+                Plot plot = FuturePlots.getInstance().getPlotByPosition(block.getLocation());
+                if(plot != null) {
+                    allowedBlocks.add(block);
+                }
+            }
+            event.setBlockList(allowedBlocks);
         }
     }
 }
