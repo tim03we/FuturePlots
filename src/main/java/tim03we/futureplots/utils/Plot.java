c02b17c002312a0318ba2331225c3c7cbf412782
tim03we <tim.wenderholm973@gmail.com>
2023-04-13 17:24:17 +0200

    ops..

--- src/main/java/tim03we/futureplots/utils/Plot.java
+++ src/main/java/tim03we/futureplots/utils/Plot.java
@@ -71,6 +71,10 @@ public class Plot {
         Server.getInstance().getScheduler().scheduleDelayedTask(FuturePlots.getInstance(), new PlotSetBorderTask(getPlot(x, z, levelName), block), 1, true);
     }
 
+    public boolean canByPass(Player player) {
+        return new PlotPlayer(player).canByPass();
+    }
+
     public boolean canByPass(Player player, Plot plot) {
         return new PlotPlayer(player).canByPass(plot);
     }
